690861055d3bd4a9ec5cd5b1dd32392e19383aed
Ana Barbosa <anabrs1@gmail.com>
2017-05-02 11:37:16 +0200

    Add files via upload

---
+++ 4 Databases in Python/week_5/Assignment 17/geoDATA/where.js
@@ -0,0 +1,17 @@
+myData = [
+[36.7245796,-4.4187323, 'Calle Ramos Marín, S/N, 29012 Málaga, Spain'],
+[36.7241881,-4.4229277, 'Plaza de San Francisco, 29008 Málaga, Spain'],
+[50.0668858,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[30.018923,31.499674, 'Parcel 8, 74 S El-Teseen St, NEW CAIRO, Cairo, Cairo Governorate 11835, Egypt'],
+[36.719939,-4.4161214, 'Av. de Cervantes, 2, 29016 Málaga, Spain'],
+[33.4515401,-112.0645774, 'The University of Arizona, 641 E Van Buren St, Phoenix, AZ 85709, USA'],
+[38.0399391,23.8030901, 'Monumental Plaza, Building C, 1st Floor, Leof. Kifisias 44, Marousi 151 25, Greece'],
+[28.3639812,75.5869784, 'Campus, Vidya Vihar, Pilani,, Pilani Road, BITS, Pilani, Rajasthan 333031, India'],
+[25.2677203,82.9912582, 'Kabir Colony, Banaras Hindu University Campus, Varanasi, Uttar Pradesh 221005, India'],
+[12.9503878,77.5022224, 'Mysore Road, Jnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.549841,-97.1143146, '1301 S University Parks Dr, Waco, TX 76798, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, BeiTaiPingZhuang, Haidian Qu, Beijing Shi, China, 100875'],
+[44.8184339,20.4575676, 'Studentski trg 1, Beograd, Serbia'],
+[42.5030333,-89.0309048, '700 College St, Beloit, WI 53511, USA'],
+[10.6779085,78.7445488, 'Palkalaiperur, Tiruchirappalli, Tamil Nadu 620024, India']
+];
